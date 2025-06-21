9d91acaae7be4e314a7cd0d5dc5ddf43789693c1
Nick Kotsakidis <nick.kotsakidis@andsafe.de>
2025-06-21 14:42:05 +0200

    gel: 7.3.0 -> 7.7.0

--- pkgs/by-name/ge/gel/package.nix
+++ pkgs/by-name/ge/gel/package.nix
@@ -16,19 +16,19 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "gel";
-  version = "7.3.0";
+  version = "7.7.0";
 
   src = fetchFromGitHub {
     owner = "geldata";
     repo = "gel-cli";
     tag = "v${version}";
-    hash = "sha256-tMbAU6tlyDcAzUQ8FK0Q0V+LgzHAazETtFuC050hObw=";
+    hash = "sha256-A4pKkWC3x+n7PXEZlvc9wL4pa/BG7kBk1vJnyTnVyek=";
     fetchSubmodules = true;
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
-    hash = "sha256-IWGOh8VbE0rCIRtiAqGlFExd1u80HWyoluquWVRaQoo=";
+    hash = "sha256-4LLGg+f8Q7jskQr/wY0eCDkn9bC+zDpMvyF5D0A1oG4=";
   };
 
   nativeBuildInputs = [
